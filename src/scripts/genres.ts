9bb540b2118010f151119ddd981356c928e913fe
lucassouza0807 <lucassouza0807@gmail.com>
2022-11-03 03:02:43 -0300

    API-KEY leak fixed

--- src/scripts/genres.ts
+++
@@ -1,7 +0,0 @@
-import useFetch from "@hooks/useFetch";
-
-export default function getGenre() {
-    const { data, error} = useFetch("https://api.themoviedb.org/3/genre/movie/list?api_key=875a606e681e096b3408b7415d5b86c5&language=en-US");
-
-    return data.results;
-}
